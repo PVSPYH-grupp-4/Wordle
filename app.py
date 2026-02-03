5e4061a679678944821df60d56e918b52b6b2162
DavidDanell <david.danell@outlook.com>
2026-02-03 15:34:33 +0100

    Done

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-# requires flask in venv
+import os
 from flask import Flask, render_template
+from seed import seedData
+from database import db
+from dotenv import load_dotenv
+from flask_migrate import Migrate
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]=os.getenv("DATABASE_URL")
+db.init_app(app)
+migrate= Migrate(app, db)
 
 # render to main page
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+    with app.app_context():
+        seedData()
